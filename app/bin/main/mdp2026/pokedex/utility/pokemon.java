ef37028d2efd1ecd2477a939c99d42a79d7748d3
Michele Grillo <michele.grillo@studenti.unicam.it>
2026-04-08 17:10:06 +0200

    prog non finuto ancora

---
+++ app/bin/main/mdp2026/pokedex/utility/pokemon.java
@@ -0,0 +1,58 @@
+package utility;
+
+public class pokemon {
+    int identificativo;
+    String nome ;
+    String tipo1;
+    String tipo2;
+    float altezza;
+    float peso;
+
+    public void setIdentificativo(int identificativo) {
+        this.identificativo = identificativo;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setTipo1(String tipo1) {
+        this.tipo1 = tipo1;
+    }
+
+    public void setTipo2(String tipo2) {
+        this.tipo2 = tipo2;
+    }
+
+    public void setAltezza(float altezza) {
+        this.altezza = altezza;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public int getIdentificativo() {
+        return identificativo;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public float getAltezza() {
+        return altezza;
+    }
+
+    public String getTipo2() {
+        return tipo2;
+    }
+
+    public String getTipo1() {
+        return tipo1;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
